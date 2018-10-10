2bd307930d75a1c19d49cb11393f768aa59a2447
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-11 00:24:05 +0530

    Added 'Login' button from material-UI on Header

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import './Home.css';
+import Header from '../../common/Header/Header';
 
 class Home extends Component {
 	render() {
 		return (
-			<div>Movie Home page</div>
+			<Header />
 		)
 	}
 }
