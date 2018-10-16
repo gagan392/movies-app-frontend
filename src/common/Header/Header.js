1b472b064a9bdee9dd8153948b8da4ef8295ec51
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-16 22:49:07 +0530

    Added username and password inside Login form

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -5,11 +5,34 @@ import Button from '@material-ui/core/Button';
 import logo from "../../assets/logo.svg";
 import Modal from "react-modal";
 import { Tab, Tabs } from '@material-ui/core';
+import Typography from "@material-ui/core/Typography";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)'
+	}
+};
+
+const TabContainer = props => {
+	return (
+		<Typography component="div" style={{ padding: 0 }}>
+			{props.children}
+		</Typography>
+	);
+}
 class Header extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state ={
+		this.state = {
 			showModal: false,
 			value: 0
 		}
@@ -45,11 +68,24 @@ class Header extends Component {
 							ariaHideApp={false}
 							isOpen={this.state.showModal}
 							contentLabel="Login"
-							onRequestClose={this.closeLoginModalHandler}>
+							onRequestClose={this.closeLoginModalHandler}
+							style={customStyles}
+						>
 							<Tabs value={this.state.value} onChange={this.loginTabChangeHandler}>
 								<Tab label="Login"></Tab>
 								<Tab label="Register"></Tab>
 							</Tabs>
+							<TabContainer>
+								<FormControl required>
+									<InputLabel htmlFor="username"> Username</InputLabel>
+									<Input id="username" type="text" />
+								</FormControl>
+
+								<FormControl required>
+									<InputLabel htmlFor="passowrd"> Passowrd</InputLabel>
+									<Input id="passowrd" type="passowrd" />
+								</FormControl>
+							</TabContainer>
 						</Modal>
 					</div>
 				</div>
