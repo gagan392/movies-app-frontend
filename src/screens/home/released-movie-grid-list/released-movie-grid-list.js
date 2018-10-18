46647d39c40b3b2a226024fb0d20de8fa5569177
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-18 23:47:54 +0530

    Replace Router Link with location.href

--- src/screens/home/released-movie-grid-list/released-movie-grid-list.js
+++ src/screens/home/released-movie-grid-list/released-movie-grid-list.js
@@ -11,17 +11,20 @@ const styles = () => ({
 });
 
 class ReleasedMovieGridList extends Component {
+
+	movieClickHandler = movieId => {
+		window.location.href = `/details/${movieId}`;
+	}
+
 	render() {
 		const { classes } = this.props;
 		return (
 			<GridList cellHeight={350} cols={4} className={classes.gridListReleasedMovies}>
 				{moviesData.map(movieData =>
-					<Link key={movieData.id} to={`/details/${movieData.id}`}>
-						<GridListTile key={movieData.id} className="released-movie-grid-item">
+						<GridListTile key={movieData.id} className="released-movie-grid-item" onClick={() => this.movieClickHandler(movieData.id)}>
 							<img src={movieData.poster_url} className="movie-poster" alt={movieData.title} />
 							<GridListTileBar title={movieData.title} />
 						</GridListTile>
-					</Link>
 				)}
 			</GridList>
 		);
