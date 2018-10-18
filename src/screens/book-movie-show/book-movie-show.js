73e2878a3d3648d75b18d63efab5822ac1ec1d27
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-19 03:10:56 +0530

    Added Location on book show page

--- src/screens/book-movie-show/book-movie-show.js
+++ src/screens/book-movie-show/book-movie-show.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import { Typography, withStyles } from '@material-ui/core';
+import { Typography, withStyles, Card, CardContent, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 import Header from '../../common/Header/Header';
 import moviesData from "../../common/movieData";
+import language from '../../common/language';
+import location from '../../common/location';
+import showDate from '../../common/showDate';
+import showTime from '../../common/showTime';
+
+import './book-movie-show.css'
 
 const styles = theme => ({
 	backButton: {
@@ -21,7 +27,8 @@ class BookMovieShow extends Component {
 	constructor() {
 		super();
 		this.state = {
-			movie: {}
+			movie: {},
+			location: {}
 		}
 	}
 
@@ -36,16 +43,40 @@ class BookMovieShow extends Component {
 
 	}
 
+	locationChangeHandler = e => {
+		this.setState({ location: e.target.value });
+	}
+
 	render() {
 		const { routeData, classes } = this.props;
 		return (
 			<>
 				<Header />
-				<div className="back">
-					<Link id={`BackButton`} to={`/details/${routeData.params.movieId}`} className={classes.backButton}>
-						<ChevronLeft />
-						<Typography style={{ display: "inline" }} variant="subheading" component="span">Back to Movie Details</Typography>
-					</Link>
+				<div className="bookShow">
+					<div className="back">
+						<Link id={`BackButton`} to={`/details/${routeData.params.movieId}`} className={classes.backButton}>
+							<ChevronLeft />
+							<Typography style={{ display: "inline" }} variant="subheading" component="span">Back to Movie Details</Typography>
+						</Link>
+					</div>
+					<Card className="cardStyle">
+						<CardContent>
+							<Typography variant="headline" component="h2"> BOOK SHOW </Typography><br />
+
+							<FormControl required className="formControl" >
+								<InputLabel htmlFor="location">Choose Location: </InputLabel>
+								<Select
+									id="location"
+									value={this.state.location}
+									onChange={this.locationChangeHandler}
+								>
+									{location.map(loc => (
+										<MenuItem key={"location" + loc.id} value={loc.location}>{loc.location}</MenuItem>
+									))}
+								</Select>
+							</FormControl>
+						</CardContent>
+					</Card>
 				</div>
 			</>
 		)
