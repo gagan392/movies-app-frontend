aaf13ea6077857dbe67a3f6db670ef792c388916
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-18 21:04:20 +0530

    Added poster, title and genres on details page

--- src/screens/movie-details/movie-detials.js
+++ src/screens/movie-details/movie-detials.js
@@ -4,6 +4,7 @@ import Header from "../../common/Header/Header";
 
 import moviesData from "../../common/movieData";
 import "./movie-detials.css";
+import { Typography } from "@material-ui/core";
 
 class MovieDetails extends Component {
 
@@ -14,22 +15,31 @@ class MovieDetails extends Component {
 		}
 	}
 
-	componentDidMount() {
+	componentWillMount() {
 		const currState = this.state;
 		currState.movie = moviesData.find(movie => {
 			return movie.id === this.props.routeData.params.movieId
 		});
 
-		this.setState({currState});
-		console.log(this.state.movie);
+		this.setState({ currState });
 	}
 	render() {
+		const { movie } = this.state;
 		return (
 			<div className="detials">
 				<Header />
 				<div className="flex-containerDetails">
-					<div className="leftDetails"></div>
-					<div className="middleDetails"></div>
+					<div className="leftDetails">
+						<img src={movie.poster_url} alt={movie.title} />
+					</div>
+					<div className="middleDetails">
+						<div>
+							<Typography variant="headline" component="h2">{movie.title}</Typography>
+						</div>
+						<div>
+							<Typography><span className="bold">Genres:</span>{movie.genres.join(',')}</Typography>
+						</div>
+					</div>
 					<div className="rightDetails"></div>
 				</div>
 			</div>
