de478688789ba9ff15a193118ada29815fd4f59c
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-19 02:31:06 +0530

    Used React Router Link to navigate to movie details page

--- src/screens/home/released-movie-grid-list/released-movie-grid-list.js
+++ src/screens/home/released-movie-grid-list/released-movie-grid-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import { GridList, withStyles, GridListTile, GridListTileBar } from "@material-ui/core";
 import moviesData from "../../../common/movieData";
 
@@ -11,19 +12,17 @@ const styles = () => ({
 
 class ReleasedMovieGridList extends Component {
 
-	movieClickHandler = movieId => {
-		window.location.href = `/details/${movieId}`;
-	}
-
 	render() {
 		const { classes } = this.props;
 		return (
 			<GridList cellHeight={350} cols={4} className={classes.gridListReleasedMovies}>
 				{moviesData.map(movieData =>
-						<GridListTile key={movieData.id} className="released-movie-grid-item" onClick={() => this.movieClickHandler(movieData.id)}>
+					<GridListTile key={movieData.id} className="released-movie-grid-item">
+						<Link key={movieData.id} to={`/details/${movieData.id}`}>
 							<img src={movieData.poster_url} className="movie-poster" alt={movieData.title} />
 							<GridListTileBar title={movieData.title} />
-						</GridListTile>
+						</Link>
+					</GridListTile>
 				)}
 			</GridList>
 		);
