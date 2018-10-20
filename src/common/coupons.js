e8590c879ecade30a42c41846e7b2238fba0de5e
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-20 14:35:27 +0530

    Added coupons data

---
+++ src/common/coupons.js
@@ -0,0 +1,17 @@
+let coupons = [{
+		id: 1,
+		code: "WEEKEND5",
+		value: 5 // discount: 100
+	},
+	{
+		id: 2,
+		code: "OFFER10PC",
+		value: 10 // discount: 200
+	},
+	{
+		id: 3,
+		code: "NEWUSER20",
+		value: 20 // discount: 400
+	}
+]
+export default coupons;
